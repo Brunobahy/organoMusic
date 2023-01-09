edde7ebcafcbe935a13e3c6621b9e73ddb8a69c7
Bruno Bahy <110056385+Brunobahy@users.noreply.github.com>
2023-01-09 13:08:26 -0300

    Upload

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX
+    return (
+        <header className="banner">
+            <h1>Organize suas músicas</h1>
+            <img src="/imagens/bkg.gif" alt="O banner principal da página"/>
+        </header>
+    )
+}
